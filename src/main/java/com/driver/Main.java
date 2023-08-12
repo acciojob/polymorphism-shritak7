0742f01d93154d3e66d7460d4872abe2dab7870e
Shriniwas Tak <shriniwastak@gmail.com>
2023-08-12 18:34:54 +0530

    Polymorphism assignment

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,24 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args){
+        product c2=new product();
+        c2.product(10,20);
+        c2.product(20,30,40);
+    }
+
+    public class product{
+        int x;
+        int y;
+        int z;
+        public int product(int x, int y) {
+            return x*y;
+        }
+        public int product(int x, int y, int z) {
+
+            return x*y*z;
+        }
+
+    }
 
 }
